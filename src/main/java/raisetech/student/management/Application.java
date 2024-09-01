5d597054e39285a5a28bcb9502dc35b76e73ebcf
ShotaSakaki <shota.s0725@gmail.com>
2024-09-01 17:39:08 +0900

    スタンダード第10回疑問点

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,22 +1,41 @@
 package raisetech.student.management;
 
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	@Autowired
+	private StudentRepository repository;
+
+	private String name = "Shota Sakaki";
+	private String age = "27";
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/webapp")
-	public String webapp(){
-		boolean isMixedCase = StringUtils.isMixedCase("目標は、Webアプリケーションが作れるようになること！");
-		return "目標は、Webアプリケーションが作れるようになること！" + isMixedCase;
+	@GetMapping("/student")
+	public String getStudent(@RequestParam String name){
+		Student student = repository.searchByName(name);
+		return student.getName() + " " + student.getAge() + "歳";
 	}
+
+	@PostMapping("/student")
+	public void	registerStudent(String name, int age){
+		repository.registerStudent(name, age);
+	}
+
+	@PostMapping("/studentName")
+	public void updateStudentName(String name){
+		this.name = name;
+	}
+
 }
